d67d75cdb95cd5730f409b5e5a1685be33c4fcc2
adjippp <42745069+adjippp@users.noreply.github.com>
2019-02-06 16:41:23 +0700

    Add files via upload

--- hari18latihan.py
+++ hari18latihan.py
@@ -29,7 +29,7 @@ def inputDataUser(nama, namaKota):
   inputan = (nama, )
   kursor.execute(perintah,inputan)
   mydb.commit()
-  return ("id Anda: "+str(getIDOrangTerakhir())+"Nama Anda: "+nama)+" "+kotaChecker(namaKota)
+  return "id Anda: "+str(getIDOrangTerakhir())+" Nama Anda: "+nama+", "+str(kotaChecker(namaKota))
 
 def kotaChecker(namaKota):
   cekKota='select nama from kotax'
@@ -38,25 +38,27 @@ def kotaChecker(namaKota):
   tampungKota=[]
   for i in range(len(hasil)):
     tampungKota.append(hasil[i][0])
-  idorang=getIDOrangTerakhir()
-  idkota=getIDKota(namaKota)
-  if (namaKota in tampungKota == True):
-      perintah = 'insert into orangkotax (id_orang,id_kota) values (%s,%s)'
-      inputan = (idorang,idkota)
-      kursor.execute(perintah,inputan)
-      mydb.commit()
-      return ("Kota "+namaKota," Sudah Terdaftar, id Kota: "+str(idkota))
-  else:
-      perintah = 'insert into kotax (nama) values (%s)'
-      inputan = (namaKota, )
-      kursor.execute(perintah,inputan)
-      mydb.commit()
-      perintah2 = 'insert into orangkotax (id_orang,id_kota) values (%s,%s)'
-      inputan2 = (idorang,idkota)
-      kursor.execute(perintah2,inputan2)
-      mydb.commit()
-      return ("Kota "+namaKota+" Akan Ditambahkan (Karena Belum Terdaftar), id Kota: "+str(idkota))
+  idorang=int(getIDOrangTerakhir())
 
-# inputnama=input("Masukkan nama Anda: ")
-# inputkota=input("Masukkan kota Anda: ")
-# print(inputDataUser(inputnama,inputkota))
+  if (namaKota in tampungKota):
+    idkota=getIDKota(namaKota)
+    perintah = 'insert into orangkotax (id_orang,id_kota) values (%s,%s)'
+    inputan = (idorang,idkota)
+    kursor.execute(perintah,inputan)
+    mydb.commit()
+    return "Kota "+namaKota," Sudah Terdaftar, id Kota: "+str(idkota)
+  else:
+    perintah = 'insert into kotax (nama) values (%s)'
+    inputan = (namaKota, )
+    kursor.execute(perintah,inputan)
+    mydb.commit()
+    idkota2=getIDKota(namaKota)
+    perintah2 = 'insert into orangkotax (id_orang,id_kota) values (%s,%s)'
+    inputan2 = (idorang,idkota2)
+    kursor.execute(perintah2,inputan2)
+    mydb.commit()
+    return "Kota "+namaKota+" Akan Ditambahkan (Karena Belum Terdaftar), id Kota: "+str(idkota2)
+
+inputnama=str(input("Masukkan nama Anda: "))
+inputkota=str(input("Masukkan kota Anda: "))
+print(inputDataUser(inputnama,inputkota))
